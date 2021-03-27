364b7dbea5e2a4b7aca5e845f3f09a081fc38dca
7hayMuller <thaynamuller88@gmail.com>
2021-03-27 17:07:48 -0300

    Insering FutureBuilder to create the loading logic

--- currency_converter_app/lib/main.dart
+++ currency_converter_app/lib/main.dart
@@ -1,16 +1,75 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=a100d601";
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']['USD']);
 
   runApp(MaterialApp(
-    home:Container()
+    home:Home()
   ));
 }
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      appBar: AppBar(
+        title:Text('Currency Converter'),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context , snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text('Loading...',
+                   style: TextStyle(
+                  color: Colors.amber,
+                  fontSize:25.0
+              ),
+                      textAlign: TextAlign.center,
+                  ),
+            );
+            default:
+              if(snapshot.hasError){
+                return Center(
+                  child: Text('ERROR :(',
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontSize:25.0
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(color:Colors.deepPurple);
+
+              }
+          }
+        }),
+    );
+  }
+}
+
+
+
+
 
