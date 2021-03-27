d9713c9ccde5f42406bb0c095a8a68779be8f700
7hayMuller <thaynamuller88@gmail.com>
2021-03-27 16:41:13 -0300

    Configuring HG BRAZIL API in the APP

---
+++ currency_converter_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=a100d601";
+
+void main() async {
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)['results']['currencies']['USD']);
+
+  runApp(MaterialApp(
+    home:Container()
+  ));
+}
+
